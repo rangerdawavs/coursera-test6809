dfaf0d36a96fb2d94ff15d570832cc6ba3e3b954
mom <andrewjacob.vargas@gmail.com>
2019-02-22 13:30:18 -0600

    'hi'

--- test1.py
+++ test1.py
@@ -3,17 +3,17 @@ import time
 import os
 with urllib.request.urlopen('http://192.168.1.187/') as response:
     i = 1
-while i==1:
-    html = response.read()
-    print(html)
-    print("done")
-    html2 = html.decode("utf-8")
-    print(html2)
-    f = open("index.html","w+")
-    f.write(html2)
-    f.close()
-    os.system("git add .")
-    os.system("git commit -m 'hi' ")
-    os.system("git push")
-    time.sleep(60)
-    print("60 seconds passed")
+    while i==1:
+        html = response.read()
+        print(html)
+        print("done")
+        html2 = html.decode("utf-8")
+        print(html2)
+        f = open("index.html","w+")
+        f.write(html2)
+        f.close()
+        os.system("git add .")
+        os.system("git commit -m 'hi' ")
+        os.system("git push")
+        time.sleep(10)
+        print("10 seconds passed")
