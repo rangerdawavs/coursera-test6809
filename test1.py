20463ffbd100d94a39f5d10dec7bccaf766b6f0c
mom <andrewjacob.vargas@gmail.com>
2019-02-22 13:08:19 -0600

    'hi'

--- test1.py
+++ test1.py
@@ -1,15 +1,18 @@
 import urllib.request
+import time
 import os
-with urllib.request.urlopen('http://192.168.1.187/') as response:
-   html = response.read()
-print(html)
-print("done")
-html2 = html.decode("utf-8")
-print(html2)
-f = open("index.html","w+")
-f.write(html2)
-f.close()
-os.system("git add .")
-os.system("git commit -m 'hi' ")
-os.system("git push")
-
+i = 1
+while i==1:
+    with urllib.request.urlopen('http://192.168.1.187/') as response:
+       html = response.read()
+    print(html)
+    print("done")
+    html2 = html.decode("utf-8")
+    print(html2)
+    f = open("index.html","w+")
+    f.write(html2)
+    f.close()
+    os.system("git add .")
+    os.system("git commit -m 'hi' ")
+    os.system("git push")
+    time.sleep(60)
