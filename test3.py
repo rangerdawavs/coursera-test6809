f2f184d36cf16015ebd3db96c22d0f2e910c80c6
mom <andrewjacob.vargas@gmail.com>
2019-02-26 11:39:18 -0600

    'hi'

--- test3.py
+++ test3.py
@@ -2,16 +2,24 @@ import urllib.request
 import time
 import os
 i = 1
+times = ["time"]
+temps = ["temperature"]
+hums = ["humidity"]
+table = [times,temps,hums]
 while i==1:
     for x in range(3):
         with urllib.request.urlopen('http://192.168.1.187/') as response:
             html = response.read()
         html2 = html.decode("utf-8")
         print(html2)
-        f = open("index.html","w+")
-        f.write(html2)
-        f.close()
         time.sleep(20)
+    a,b,c,d,e,f = html2.split("x")
+    temps.append(b)
+    hums.append(d)
+    html3 = ''.join([a,b,c,d,e,f])
+    f = open("index.html","w+")
+    f.write(html3)
+    f.close()
     os.system("git add .")
     os.system("git commit -m 'hi' ")
     os.system("git push")
